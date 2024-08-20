a5179933373eade8369f690fa3720f867cf19120
remorses <beats.by.morse@gmail.com>
2024-08-20 16:10:49 +0200

    more tests for use

--- spiceflow/src/types.test.ts
+++ spiceflow/src/types.test.ts
@@ -5,7 +5,7 @@ import { z } from 'zod'
 import { createSpiceflowClient } from './client/index.js'
 import { Prettify } from './elysia-fork/types.js'
 
-test('use on non Spiceflow return', async () => {
+test('`use` on non Spiceflow return', async () => {
 	function nonSpiceflowReturn() {
 		return new Spiceflow() as any
 	}
@@ -22,7 +22,29 @@ test('use on non Spiceflow return', async () => {
 	// @ts-expect-error
 	client.something
 
-	// client.xxx.post()
+	client.xxx.post()
+	expect(res.status).toBe(200)
+	expect(await res.json()).toEqual('hi')
+})
+test('`use` on Spiceflow return', async () => {
+	function nonSpiceflowReturn() {
+		return new Spiceflow().post('/usePost', () => 'hi')
+	}
+	const app = new Spiceflow()
+		.use(nonSpiceflowReturn())
+		.post('/xxx', () => 'hi')
+	const res = await app.handle(
+		new Request('http://localhost/xxx', { method: 'POST' }),
+	)
+
+	let client = createSpiceflowClient(app)
+	client.xxx.post()
+	client.usePost.post()
+	
+	type ClientType = Prettify<typeof client>
+	// @ts-expect-error
+	client.something
+
 	expect(res.status).toBe(200)
 	expect(await res.json()).toEqual('hi')
 })
