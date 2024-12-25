3f6d9b6a1bf09f5bde0765e6a2df1fda211eb893
remorses <beats.by.morse@gmail.com>
2024-12-25 19:46:58 +0100

    bundling of sdk works

--- spiceflow/scripts/play-sdk.vite.ts
+++ spiceflow/scripts/play-sdk.vite.ts
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
+    outDir: './dist-play-sdk',
     lib: {
       entry: './scripts/play-sdk.ts',
       formats: ['es'],
+
       fileName: () => 'play-sdk.js',
     },
+    rollupOptions: {
+      external: ['node-fetch'],
+    },
+    minify: false,
   },
 })
