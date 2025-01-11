7ac6282df42bc188b805e0e1f0fe0bec36d27656
MentatBot <160964065+MentatBot@users.noreply.github.com>
2025-01-11 00:22:20 +0000

    Update gitignore

--- spiceflow/src/stream.test.ts
+++ spiceflow/src/stream.test.ts
@@ -1,344 +1,102 @@
-import { describe, it, expect } from 'vitest'
-
-import { createParser } from 'eventsource-parser'
-
-import { Spiceflow } from './spiceflow.js'
-
-import { req, sleep } from './utils.js'
-
-function textEventStream(items: string[]) {
-  return items
-    .map((item) => `event: message\ndata: ${JSON.stringify(item)}\n\n`)
-    .join('')
-}
-
-function parseTextEventStreamItem(item: string) {
-  const data = item.split('data: ')[1].split('\n')[0]
-  return JSON.parse(data)
-}
+import { describe, expect, it } from 'vitest'
+import { Stream } from './stream'
 
 describe('Stream', () => {
-  it('handle stream', async () => {
-    const expected = ['a', 'b', 'c']
+    it('stop stream on canceled request', async () => {
+        const controller = new AbortController()
+        const signal = controller.signal
 
-    const app = new Spiceflow().get('/', async function* () {
-      yield 'a'
-      await sleep(10)
-
-      yield 'b'
-      await sleep(10)
-
-      yield 'c'
-    })
-
-    const response = await app
-      .handle(req('/'))
-      .then((x) => x.body)
-      .then((x) => {
-        if (!x) return
-
-        const reader = x?.getReader()
-
-        let acc = ''
-        const { promise, resolve } = Promise.withResolvers()
-
-        reader.read().then(function pump({ done, value }): unknown {
-          if (done) return resolve(acc)
-
-          expect(parseTextEventStreamItem(value.toString())).toBe(
-            expected.shift()!,
-          )
-
-          acc += value.toString()
-          return reader.read().then(pump)
+        const stream = new Stream({
+            signal,
+            async *[Symbol.asyncIterator]() {
+                yield 'hello'
+                await new Promise((r) => setTimeout(r, 100))
+                yield 'world'
+            },
         })
 
-        return promise
-      })
+        const body = stream.toReadableStream()
+        const reader = body?.getReader()!
 
-    expect(expected).toHaveLength(0)
-    expect(response).toBe(textEventStream(['a', 'b', 'c']))
-  })
-  it('handle errors after yield', async () => {
-    const app = new Spiceflow().get('/', async function* () {
-      yield 'a'
-      await sleep(10)
-
-      throw new Error('an error')
-    })
-
-    const response = await app.handle(req('/')).then((x) => x.text())
-
-    expect(response).toBe(
-      'event: message\ndata: "a"\n\nevent: error\ndata: "an error"\n\n',
-    )
-  })
-
-  it('handle errors before yield when aot is false', async () => {
-    const app = new Spiceflow()
-      .onError(({ error }) => {
-        return new Response(error.message)
-      })
-      .get('/', async function* () {
-        throw new Error('an error xxxx')
-      })
-
-    const response = await app.handle(req('/')).then((x) => x.text())
-
-    expect(response).toContain('an error')
-  })
-
-  it.todo('handle errors before yield when aot is true', async () => {
-    const app = new Spiceflow()
-      .onError(({ error }) => {
-        return new Response(error.message)
-      })
-      .get('/', async function* () {
-        throw new Error('an error')
-      })
-
-    const response = await app.handle(req('/')).then((x) => x.text())
-
-    expect(response).toContain('an error')
-  })
-
-  it.todo('handle errors before yield with onError', async () => {
-    const expected = 'error expected'
-    const app = new Spiceflow()
-      .onError(({}) => {
-        return new Response(expected)
-      })
-      .get('/', async function* () {
-        throw new Error('an error')
-      })
-
-    const response = await app.handle(req('/')).then((x) => x.text())
-
-    expect(response).toBe(expected)
-  })
-
-  it('stop stream on canceled request', async () => {
-    const expected = ['a', 'b']
-
-    const app = new Spiceflow().get('/', async function* () {
-      yield 'a'
-      await sleep(10)
-
-      yield 'b'
-      await sleep(10)
-
-      yield 'c'
-    })
-
-    const controller = new AbortController()
-
-    setTimeout(() => {
-      controller.abort()
-    }, 15)
-
-    const response = await app
-      .handle(
-        new Request('http://e.ly', {
-          signal: controller.signal,
-        }),
-      )
-      .then((x) => x.body)
-      .then((x) => {
-        if (!x) return
-
-        const reader = x?.getReader()
-
-        let acc = ''
-        const { promise, resolve } = Promise.withResolvers()
-
-        reader.read().then(function pump({ done, value }): unknown {
-          if (done) {
-            return resolve(acc)
-          }
-
-          expect(parseTextEventStreamItem(value.toString())).toBe(
-            expected.shift()!,
-          )
-
-          acc += value.toString()
-          return reader.read().then(pump)
+        const text = await new Promise((resolve) => {
+            let acc = ''
+            let resolvePromise;
+            const promise = new Promise(r => {
+                resolvePromise = r;
+            });
+            reader.read().then(function pump({ done, value }): unknown {
+                if (done) {
+                    resolve(acc)
+                    return
+                }
+                acc += new TextDecoder().decode(value)
+                return reader.read().then(pump)
+            })
+            setTimeout(() => {
+                controller.abort()
+            }, 10)
+            return promise
         })
 
-        return promise
-      })
-
-    expect(expected).toHaveLength(0)
-    expect(response).toBe(textEventStream(['a', 'b']))
-  })
-
-  // it('mutate set before yield is called', async () => {
-  // 	const expected = ['a', 'b', 'c']
-
-  // 	const app = new Spiceflow().get('/', function* () {
-  // 		set.headers['access-control-allow-origin'] = 'http://saltyaom.com'
-
-  // 		yield 'a'
-  // 		yield 'b'
-  // 		yield 'c'
-  // 	})
-
-  // 	const response = await app.handle(req('/')).then((x) => x.headers)
-
-  // 	expect(response.get('access-control-allow-origin')).toBe(
-  // 		'http://saltyaom.com'
-  // 	)
-  // })
-  it('handle stream with objects', async () => {
-    const objects = [
-      { message: 'hello' },
-      { response: 'world' },
-      { data: [1, 2, 3] },
-      { result: [4, 5, 6] },
-    ]
-    const app = new Spiceflow().get('/', async function* ({}) {
-      for (const obj of objects) {
-        yield obj
-      }
+        expect(text).toBe('hello')
     })
 
-    const body = await app.handle(req('/')).then((x) => x.body)
-
-    let events = [] as any[]
-    const parser = createParser({
-      onEvent: (event) => {
-        events.push(event)
-      },
-    })
-    const { promise, resolve } = Promise.withResolvers<void>()
-    const reader = body?.getReader()!
-
-    reader.read().then(function pump({ done, value }): unknown {
-      if (done) {
-        return resolve()
-      }
-      const text = value.toString()
-      parser.feed(text)
-      return reader.read().then(pump)
-    })
-    await promise
-
-    expect(events.map((x) => x.data)).toEqual(
-      objects.map((x) => JSON.stringify(x)),
-    )
-  })
-
-  // it('mutate set before yield is called', async () => {
-  // 	const expected = ['a', 'b', 'c']
-
-  // 	const app = new Spiceflow().get('/', function* () {
-  // 		set.headers['access-control-allow-origin'] = 'http://saltyaom.com'
-
-  // 		yield 'a'
-  // 		yield 'b'
-  // 		yield 'c'
-  // 	})
-
-  // 	const response = await app.handle(req('/')).then((x) => x.headers)
-
-  // 	expect(response.get('access-control-allow-origin')).toBe(
-  // 		'http://saltyaom.com'
-  // 	)
-  // })
-
-  // it('async mutate set before yield is called', async () => {
-  // 	const expected = ['a', 'b', 'c']
-
-  // 	const app = new Spiceflow().get('/', async function* () {
-  // 		set.headers['access-control-allow-origin'] = 'http://saltyaom.com'
-
-  // 		yield 'a'
-  // 		yield 'b'
-  // 		yield 'c'
-  // 	})
-
-  // 	const response = await app.handle(req('/')).then((x) => x.headers)
-
-  // 	expect(response.get('access-control-allow-origin')).toBe(
-  // 		'http://saltyaom.com'
-  // 	)
-  // })
-
-  it('return value if not yield', async () => {
-    const app = new Spiceflow()
-      .get('/', function* () {
-        return 'hello'
-      })
-      .get('/json', function* () {
-        return { hello: 'world' }
-      })
-
-    const response = await Promise.all([
-      app.handle(req('/')),
-      app.handle(req('/json')),
-    ])
-
-    expect(await response[0].text()).toBe('"hello"')
-    expect(await response[1].json()).toEqual({
-      hello: 'world',
-    })
-  })
-
-  it('return async value if not yield', async () => {
-    const app = new Spiceflow()
-      .get('/', function* () {
-        return 'hello'
-      })
-      .get('/json', function* () {
-        return { hello: 'world' }
-      })
-
-    const response = await Promise.all([
-      app.handle(req('/')),
-      app.handle(req('/json')),
-    ])
-
-    expect(await response[0].text()).toBe('"hello"')
-    expect(await response[1].json()).toEqual({
-      hello: 'world',
-    })
-  })
-
-  it('handle object and array', async () => {
-    const expected = [{ a: 'b' }, ['a'], ['a', 1, { a: 'b' }]]
-    const expectedResponse = JSON.stringify([...expected])
-    let i = 0
-
-    const app = new Spiceflow().get('/', async function* () {
-      yield expected[0]
-      await sleep(10)
-
-      yield expected[1]
-      await sleep(10)
-
-      yield expected[2]
-    })
-
-    const response = await app
-      .handle(req('/'))
-      .then((x) => x.body)
-      .then((x) => {
-        if (!x) return
-
-        const reader = x?.getReader()
-
-        const { promise, resolve } = Promise.withResolvers<void>()
-
-        reader.read().then(function pump({ done, value }): unknown {
-          if (done) return resolve()
-
-          expect(parseTextEventStreamItem(value.toString())).toEqual(
-            expected[i++],
-          )
-
-          return reader.read().then(pump)
+    it('handle stream with objects', async () => {
+        const stream = new Stream({
+            async *[Symbol.asyncIterator]() {
+                yield { hello: 'world' }
+                yield { ciao: 'mondo' }
+            },
         })
+        const body = stream.toReadableStream({
+            transform: (x) => {
+                return JSON.stringify(x) + '\n'
+            },
+        })
+        let resolvePromise;
+        const promise = new Promise<void>(r => {
+            resolvePromise = r;
+        });
+        const reader = body?.getReader()!
+        reader.read().then(function pump({ done, value }): unknown {
+            if (done) {
+                resolvePromise()
+                return
+            }
+            const text = new TextDecoder().decode(value)
+            expect(text).toMatch(/\n$/)
+            return reader.read().then(pump)
+        })
+        await promise
+    })
 
-        return promise
-      })
-  })
+    it('handle object and array', async () => {
+        const x = new Stream({
+            async *[Symbol.asyncIterator]() {
+                yield { hello: 'world' }
+                yield [1, 2, 3]
+            },
+        }).toReadableStream({
+            transform: (x) => {
+                return JSON.stringify(x) + '\n'
+            },
+        })
+        await new Promise((resolve) => {
+            const reader = x?.getReader()
+            let resolvePromise;
+            const promise = new Promise<void>(r => {
+                resolvePromise = r;
+            });
+            reader.read().then(function pump({ done, value }): unknown {
+                if (done) {
+                    resolve()
+                    return
+                }
+                const text = new TextDecoder().decode(value)
+                expect(text).toMatch(/\n$/)
+                return reader.read().then(pump)
+            })
+            return promise
+        })
+    })
 })
