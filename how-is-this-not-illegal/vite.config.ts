9ae786c62d4331e45e7e1289772a1e4ce2111fe1
Tommy D. Rossi <beats.by.morse@gmail.com>
2026-03-13 17:08:53 +0100

    Update vite.config.ts

--- how-is-this-not-illegal/vite.config.ts
+++ how-is-this-not-illegal/vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from "vite";
-import { spiceflowPlugin } from "spiceflow/vite";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import { spiceflowPlugin } from 'spiceflow/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-	clearScreen: false,
-	plugins: [
-		// inspect(),
-		spiceflowPlugin({
-			entry: "./app/main.tsx",
-		}),
-		tailwindcss(),
-	],
-});
+  clearScreen: false,
+  plugins: [
+    // inspect(),
+    spiceflowPlugin({
+      entry: './app/main.tsx',
+    }),
+    tailwindcss(),
+  ],
+})
